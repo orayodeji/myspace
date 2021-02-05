524c4438de3f13879c3e33758089e6c2836efb9d
orayodeji <orayodeji@gmail.com>
2021-02-05 11:51:24 +0100

    in progress

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links li')
+
+const summary = document.querySelector('.summary')
+const photo = document.querySelector('.photo')
+
+
+
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('open')
+    links.forEach(link =>{
+        link.classList.toggle('fade')
+    })
+})
+
+
+    
+
